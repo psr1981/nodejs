1f50339ff07e88ae36d7ac60b71525cb44599e39
pradeep singh <pradeep.s.rajpoot@gmail.com>
2018-08-08 17:18:26 +0530

    adding rest api test code

--- expressjs/hello-server.js
+++ expressjs/hello-server.js
@@ -3,11 +3,16 @@ var app = express()
 var bodyParser = require('body-parser')
 var multer = require('multer')
 var cookieParser = require('cookie-parser')
+var morgan=require('morgan')
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(multer({ dest: '/tmp/'}));
 app.use(cookieParser());
+app.use(multer({ dest: './cache'}).any());
+app.use(function(req, res, next) {
+    morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[Content-Length] ":referrer" ":user-agent" ":compression"')(req, res, next);
+});
+
 
 app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
@@ -17,6 +22,7 @@ app.get('/', (req, res) => {
 	res.send('Hello World')
 })
 
+
 var server = app.listen(8081, () => {
 	console.log(server.address())
 	var host = server.address().address;
@@ -24,3 +30,5 @@ var server = app.listen(8081, () => {
 
 	console.log('server is listening at http://'+ host+ ':'+ port)
 })
+
+
